24715835711961a615e06f8862ed2803c81988a4
« Ephely » <ephely1@gmail.com>
2026-08-23 23:53:34 +0200

    Task 3

---
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -0,0 +1,50 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+  test('renders h1 element with text "School dashboard"', () => {
+    render(<App />);
+    const headingElement = screen.getByRole('heading', {
+      name: /school dashboard/i,
+      level: 1,
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('renders paragraph elements in App-body and App-footer with correct text', () => {
+    render(<App />);
+
+    const bodyParagraph = screen.getByText(
+      /login to access the full dashboard/i,
+    );
+    expect(bodyParagraph).toBeInTheDocument();
+
+    const footerParagraph = screen.getByText(/copyright/i);
+    expect(footerParagraph).toBeInTheDocument();
+  });
+
+  test('renders logo image with correct alt text', () => {
+    render(<App />);
+    const logoImage = screen.getByAltText(/holberton logo/i);
+    expect(logoImage).toBeInTheDocument();
+  });
+
+  test('renders Notifications component', () => {
+    render(<App />);
+    const notificationText = screen.getByText(
+      /here is the list of notifications/i,
+    );
+    expect(notificationText).toBeInTheDocument();
+  });
+
+  test('renders 2 input elements, 2 label elements, and 1 button element', () => {
+    render(<App />);
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    const buttonElement = screen.getByRole('button', { name: /ok/i });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
