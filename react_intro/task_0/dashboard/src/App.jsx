596d61fa6eda627f4668a28ff54474f3e2e3905a
« Ephely » <ephely1@gmail.com>
2026-08-23 22:25:49 +0200

    Task 0

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,23 @@
+import HolbertonLogo from './assets/holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={HolbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {currentYear} - Holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
