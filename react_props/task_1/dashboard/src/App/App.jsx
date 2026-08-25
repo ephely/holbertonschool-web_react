4bb47b55e032b359b8d5a86e479f5545e8680937
« Ephely » <ephely1@gmail.com>
2026-08-25 14:11:20 +0200

    Task 1

---
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ react_props/task_1/dashboard/src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import HolbertonLogo from '../assets/holberton-logo.jpg';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="App-header">
+            <img src={HolbertonLogo} alt="holberton logo" />
+            <h1>School dashboard</h1>
+        </div>
+    );
+}
+
+export default Header;
