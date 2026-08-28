b9ae1799cf0a0acf4ea8dd68511a82c1b58c0f95
« Ephely » <ephely1@gmail.com>
2026-08-28 02:12:12 +0200

    Task 1 fixed

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -1,38 +1,39 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
 
 describe('Login component', () => {
-    test('renders without crashing', () => {
-        render(<Login />);
-    });
+  test('renders without crashing', () => {
+    render(<Login />);
+  });
 
-    test('renders 2 labels, 2 inputs, and 1 button', () => {
-        render(<Login />);
-        const emailLabel = screen.getByText(/email/i);
-        const passwordLabel = screen.getByText(/password/i);
-        const emailInput = screen.getByLabelText(/email/i);
-        const passwordInput = screen.getByLabelText(/password/i);
-        const buttonElement = screen.getByRole('button', { name: /ok/i });
+  test('renders 2 label, 2 input, and 1 button elements', () => {
+    render(<Login />);
+    const emailLabel = screen.getByText(/email/i);
+    const passwordLabel = screen.getByText(/password/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    const buttonElement = screen.getByRole('button', { name: /ok/i });
 
-        expect(emailLabel).toBeInTheDocument();
-        expect(passwordLabel).toBeInTheDocument();
-        expect(emailInput).toBeInTheDocument();
-        expect(passwordInput).toBeInTheDocument();
-        expect(buttonElement).toBeInTheDocument();
-    });
+    expect(emailLabel).toBeInTheDocument();
+    expect(passwordLabel).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+  });
 
-    test('focuses input element when related label is clicked', () => {
-        render(<Login />);
-        const emailLabel = screen.getByText(/email/i);
-        const emailInput = screen.getByLabelText(/email/i);
+  test('focuses input element when related label is clicked', async () => {
+    const user = userEvent.setup();
+    render(<Login />);
 
-        fireEvent.click(emailLabel);
-        expect(emailInput).toHaveFocus();
+    const emailLabel = screen.getByText(/email/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    await user.click(emailLabel);
+    expect(emailInput).toHaveFocus();
 
-        const passwordLabel = screen.getByText(/password/i);
-        const passwordInput = screen.getByLabelText(/password/i);
-
-        fireEvent.click(passwordLabel);
-        expect(passwordInput).toHaveFocus();
-    });
+    const passwordLabel = screen.getByText(/password/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    await user.click(passwordLabel);
+    expect(passwordInput).toHaveFocus();
+  });
 });
